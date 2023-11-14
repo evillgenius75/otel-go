22768ee158f36cb29f3e8e18fe721e9b15da009a
Eddie Villalba <eddie@evillgenius.tech>
2023-11-14 16:57:06 -0600

    updated a print statement

--- main.go
+++ main.go
@@ -2,34 +2,34 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"sync"
-	"strconv"
 	"log"
+	"net/http"
+	"strconv"
+	"sync"
 )
 
 var counter int
 var mutex = &sync.Mutex{}
 
 func echoString(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello")
+	fmt.Fprintf(w, "hello")
 }
 
 func incrementCounter(w http.ResponseWriter, r *http.Request) {
-    mutex.Lock()
-    counter++
-    fmt.Fprintf(w, strconv.Itoa(counter))
-    mutex.Unlock()
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
 }
 
-func  main() {
-    http.HandleFunc("/", echoString)
+func main() {
+	http.HandleFunc("/", echoString)
 
-    http.HandleFunc("/increment", incrementCounter)
+	http.HandleFunc("/increment", incrementCounter)
 
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hi")
-    })
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi, this server has been access %d times", counter)
+	})
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
